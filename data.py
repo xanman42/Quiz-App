b382802d4c24cad8980dcbb58d20a3c0e130d520
Xander Kirsten <xanderkirsten1@gmail.com>
2022-11-05 15:19:15 +0200

    data api

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
-question_data =
+import requests
+
+parameters = {
+    'amount': 20,
+    'category': 18,
+    'type': 'boolean'
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()['results']
