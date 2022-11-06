fd77d9c7067a6333e48d39aaef9f23c2ac454683
Xander Kirsten <xanderkirsten1@gmail.com>
2022-11-06 12:14:13 +0200

    improving styling and format,deleting unnecessary code

--- main.py
+++ main.py
@@ -14,14 +14,3 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 ui = QuizUI(quiz)
 
-#ui.canvas.itemconfig(ui.text, text=quiz.next_question())
-#ui.true_but.config(command=quiz.check_answer('true'))
-#ui.false_but.config(command=quiz.check_answer('false'))
-
-
-#while quiz.still_has_questions():
-#quiz.next_question()
-
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
